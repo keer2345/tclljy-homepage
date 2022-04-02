2de4944cc09fc21e8d3358c5301f2523b014a03b
keer2345 <keer2345@gmail.com>
2022-04-02 11:35:46 +0800

    config

--- .umirc.ts
+++ .umirc.ts
@@ -1,9 +1,12 @@
+import routes from './config/routes'
 import { defineConfig } from 'umi'
 
 export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-  routes: [{ path: '/', component: '@/pages/index' }],
+  title: '同城蓝领家园',
+  history: { type: 'browser' },
+  routes: routes,
   fastRefresh: {},
 })
