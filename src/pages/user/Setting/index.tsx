207b732a83157ac14cbc4ae98e44b865c3416805
keer2345 <keer2345@gmail.com>
2022-04-18 22:30:16 +0800

    个人资料

--- src/pages/user/Setting/index.tsx
+++ src/pages/user/Setting/index.tsx
@@ -1,7 +1,7 @@
 import KrCarouselImage from '@/components/KrCarouselImage'
 import React, { useEffect, useState } from 'react'
 import ProCard from '@ant-design/pro-card'
-import { Avatar, Image, Button, Breadcrumb } from 'antd'
+import { Avatar, Image, Button, Breadcrumb, Row, Col, Space } from 'antd'
 import { Link, history } from 'umi'
 import {
   HomeOutlined,
@@ -15,6 +15,18 @@ import {
   LeftOutlined,
 } from '@ant-design/icons'
 import SettingLeft from './SettingLeft'
+import ProForm, {
+  ModalForm,
+  DrawerForm,
+  QueryFilter,
+  LightFilter,
+  StepsForm,
+  ProFormText,
+  ProFormDateRangePicker,
+  ProFormSelect,
+  ProFormRadio,
+  LoginForm,
+} from '@ant-design/pro-form'
 
 const Setting = () => {
   const [userInfo, setUserInfo] = useState(0)
@@ -33,6 +45,10 @@ const Setting = () => {
   useEffect(() => {
     setUserInfo(JSON.parse(localStorage.getItem('userInfo') || '{}'))
   }, [])
+  const formItemLayout = {
+    labelCol: { span: 4 },
+    wrapperCol: { span: 14 },
+  }
   return (
     <>
       <KrCarouselImage />
@@ -46,11 +62,63 @@ const Setting = () => {
           <SettingLeft userInfo={userInfo} />
         </ProCard>
 
-        <ProCard
-          // title="　"
-          headerBordered
-        >
-          setting
+        <ProCard title="个人资料" headerBordered>
+          <div
+            style={{
+              margin: 24,
+            }}
+          >
+            <ProForm
+              labelWidth="auto"
+              onFinish={async (values: any) => {
+                await waitTime(2000)
+                console.log(values)
+                message.success('提交成功')
+              }}
+              layout={'horizontal'}
+              size={'large'}
+              initialValues={
+                {
+                  // username: userInfo.username,
+                }
+              }
+              {...formItemLayout}
+              submitter={{
+                render: (props, doms) => {
+                  return (
+                    <Row>
+                      <Col span={14} offset={4}>
+                        <Space>{doms}</Space>
+                      </Col>
+                    </Row>
+                  )
+                },
+              }}
+            >
+              <ProFormText
+                width="md"
+                name="username"
+                label="账号"
+                tooltip="账号注册后不可更改"
+                placeholder={userInfo.username}
+                // initialValue={userInfo.username}
+                disabled
+              />
+              <ProFormText
+                width="md"
+                name="realname"
+                label="姓名"
+                tooltip="可输入您的真实姓名"
+                placeholder={userInfo.realname || '请输入姓名'}
+              />
+              <ProFormText
+                width="md"
+                name="identity"
+                label="身份证号码"
+                placeholder={userInfo.identity || '请输入身份证号码'}
+              />
+            </ProForm>
+          </div>
         </ProCard>
       </ProCard>
     </>
